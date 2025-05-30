ee157da30bb39f3fd6d7bee77ceee57806b5d91f
kuche1 <znamsiparolata4esno@abv.bg>
2025-05-30 20:34:28 +0300

    update

---
+++ scripting/BuildACS.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+ME=$(readlink -e -- "$BASH_SOURCE")
+HERE=$(dirname -- "$ME")
+
+# how to get the compiler:
+# download a build from https://www.sourcemod.net/downloads.php
+# unarchive to `local-sourcemod` in the parent folder
+
+{
+    cd "$HERE"
+
+    # compile ACS
+    ./../local-sourcemod/addons/sourcemod/scripting/spcomp ./acs.sp -v:0
+
+    cd -
+}
+
+echo 'you can now copy the compiled ".smx" file into "left4dead2/addons/sourcemod/plugins"'
