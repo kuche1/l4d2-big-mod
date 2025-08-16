015018c54328f9e53c18b6de57c054ee18afd5ea
kuche1 <znamsiparolata4esno@abv.bg>
2025-08-17 00:42:40 +0300

    update

--- build.sh
+++ build.sh
@@ -5,30 +5,27 @@ set -euo pipefail
 ME=$(readlink -e -- "$BASH_SOURCE")
 HERE=$(dirname -- "$ME")
 
+COMPILER="$HERE/sourcemod/addons/sourcemod/scripting/spcomp"
+
+INPUT="$HERE/src/main.sp"
+OUTPUT="$HERE/big_mod.smx"
+
 {
     cd "$HERE"
 
-    compiler='./sourcemod/addons/sourcemod/scripting/spcomp'
-
-    if [ ! -f "$compiler" ]; then
-        echo "The compiler needs to be present at the following location: $compiler"
+    if [ ! -f "$COMPILER" ]; then
+        echo "The compiler needs to be present at the following location: $COMPILER"
         echo 'You can download it from https://www.sourcemod.net/downloads.php'
         exit 1
     fi
 
     # mark compiler as executable
-    chmod +x "$compiler"
-
-    if [ -d "/etc/nix" ]; then
-        nixos_fixer="steam-run"
-    else
-        nixos_fixer=""
-    fi
+    chmod +x "$COMPILER"
 
     # compile ACS
-    "$nixos_fixer" "$compiler" ./scripting/campaign_switcher.sp -v:0
+    "$COMPILER" "$INPUT" -v:0 -o "$OUTPUT"
 
     cd -
 }
 
-echo 'you can now copy the compiled ".smx" file into "left4dead2/addons/sourcemod/plugins"'
+echo "Compilation done - you can now copy the compiled \"$OUTPUT\" file into \"left4dead2/addons/sourcemod/plugins\""
---
+++ src/main.sp
@@ -0,0 +1,23 @@
+
+#define PLUGIN_VERSION "v0.0-dev0"
+
+#pragma semicolon 1
+#pragma newdecls required
+
+#include <sourcemod>
+#include <sdktools>
+
+// #include "settings.sp"
+
+public Plugin myinfo =
+{
+    name        = "Big Mod/",
+    author      = "kuche1",
+    description = "Allows for voting for campaigns",
+    version     = PLUGIN_VERSION,
+    url         = "https://github.com/kuche1/l4d2-big-mod",
+}
+
+public void OnPluginStart()
+{
+}
